07a3a88460fc057f62aa9e7dfbc94b1b3a59fc9a
Andy Pai <andy@r2pi.co>
2026-03-30 12:07:18 -0400

    chore: tighten local quality checks

--- src/cli.ts
+++ src/cli.ts
@@ -1,5 +1,5 @@
 #!/usr/bin/env bun
-import { run } from './index.js'
+import { run } from './index'
 
 run(process.argv.slice(2)).catch((err) => {
   // Commander throws with exitCode for --help, parse errors, etc.
